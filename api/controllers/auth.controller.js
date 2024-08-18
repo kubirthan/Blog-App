767249778f62bf0d979e7d4703f22c6359cc5313
kubirthan <kubirthan7991@gmail.com>
2024-08-18 21:35:11 +0530

    Create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
-import bcrypt from  'bcryptjs'
+import bcryptjs from  'bcryptjs'
 import { errorHandler } from "../utils/error.js"
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body
@@ -9,7 +10,7 @@ export const signup = async (req, res, next) => {
         return next(errorHandler(400, 'All fields are required'))
     }
 
-    const hashPassword = bcrypt.hashSync(password, 10)
+    const hashPassword = bcryptjs.hashSync(password, 10)
 
     const newUser = new User({
         username,
@@ -23,6 +24,34 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
 
-    
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body
+
+    if(!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404, 'user not found'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword){
+            return next(errorHandler(404, 'Invalid password'))
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const { password:pass, ...rest} = validUser._doc
+        res 
+          .status(200)
+          .cookie('access_token', token, {
+            httpOnly: true
+          })
+          .json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,5 +4,6 @@ import { signup } from '../controllers/auth.controller.js'
 const router = express.Router()
 
 router.post('/signup', signup )
+router.post('/signin', signin)
 
 export default router
