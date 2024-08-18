fd454e1fe653e571dfc5450d63e47ecae5eb07f7
kubirthan <kubirthan7991@gmail.com>
2024-08-18 20:07:04 +0530

    Complete the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp/>} />
         <Route path='/projects' element={<Projects/>} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {BsFacebook, BsGithub, BsInstagram, BsTwitter, BsDribbble} from 'react-icons/bs'
+
+export default function FooterCom() {
+  return (
+  <Footer container className="border border-t-8 border-teal-500">
+    <div className="w-full max-w-7xl mx-auto">
+      <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+        <div className="mt-5">
+          <Link to={"/"} className="self-center whitespace-nowrap text-lg sm:text-xl font-bold dark:text-white ">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Km
+            </span>
+            Blog
+          </Link>
+        </div>
+        <div className="grid grid-cols-2 gap-6 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+            <Footer.Title title="About"/>
+            <Footer.LinkGroup col>
+                <Footer.Link
+                    href="https://www.100projects.com"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    100 35 Projects
+                </Footer.Link>
+                <Footer.Link
+                    href='/about' 
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Km Blog
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+            <div>
+            <Footer.Title title="follow us"/>
+            <Footer.LinkGroup col>
+                <Footer.Link
+                    href="https://www.github.com/kubirthan"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    Github
+                </Footer.Link>
+                <Footer.Link
+                    href='#'>
+                    Discord
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+            <div>
+            <Footer.Title title="Legal"/>
+            <Footer.LinkGroup col>
+                <Footer.Link
+                    href="#">
+                    Privacy Policy
+                </Footer.Link>
+                <Footer.Link
+                    href='#' >
+                    terms &amp; Conditions
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+        </div>
+      </div>
+      <Footer.Divider />
+      <div className="w-full sm:flex sm:items-center sm:justify-between">
+        <Footer.Copyright href="#" by="Km blog" year={new Date().getFullYear()}/>
+        <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook}/>
+            <Footer.Icon href="#" icon={BsInstagram}/>
+            <Footer.Icon href="#" icon={BsTwitter}/>
+            <Footer.Icon href="#" icon={BsGithub}/>
+            <Footer.Icon href="#" icon={BsDribbble}/>
+        </div>
+      </div>
+    </div>
+  </Footer>
+  )
+}
