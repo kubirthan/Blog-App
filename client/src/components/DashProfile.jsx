3543ade3210f5cf586d09198e780660133c265c1
kubirthan <kubirthan7991@gmail.com>
2024-08-27 15:43:12 +0530

    complete profile page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function DashProfile() {
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md rounded-full'>
+          <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
+        </div>
+        <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
+        <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
+        <TextInput type='password' id='password' placeholder='password' defaultValue='********'/>
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
+          Update
+        </Button>
+        <div className='text-red-500 flex justify-between mt-5'>
+          <span className='cursor-pointer'>Delete Account</span>
+          <span className='cursor-pointer'>Sign Out</span>
+        </div>
+      </form>
+    </div>
   )
 }
